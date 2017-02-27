6cf0628d5512b3bfd94ba20a11785eb6a05471cb
Erica Lauer Vose <elauervose@gmail.com>
2017-02-27 11:21:48 -0800

    add password to renter page

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,3 +1,39 @@
+
+$(document).ready(function() {
+  "use strict";
+
+  // NOTE: This is not meant to be secure!
+  var password = "doglady";
+
+  // To clear your localStorage for testing: `localStorage.setItem("passwordMatched", "")`
+  // To check your localStorage: `localStorage.getItem("passwordMatched")`
+  // Or in the chrome settings, clear out "Cookies and other site and plugin data"
+
+  // Automatically show the existing client views if they've entered the password before
+  if (localStorage.getItem('passwordMatched') === "true") {
+		$("#passwordSubmitWrapper").addClass("hide");
+		$("#new-renter").addClass("hide");
+		$("#current-renter").removeClass("hide");
+  }
+
+  // Enable the password dialog
+  $("#passwordSubmit").on("submit", function (e) {
+  	e.preventDefault();
+
+  	// If the password matches, show the existing client views
+  	if ($("input[type=password]", e.target).val() == password) {
+  		$("#passwordSubmitWrapper").addClass("hide");
+  		$("#new-renter").addClass("hide");
+  		$("#current-renter").removeClass("hide");
+
+  		// In the future, don't show the password prompt, just let them in.
+  		localStorage.setItem('passwordMatched', true);
+  	}
+
+  	return false;
+  });
+});
+
 // // LEAVE ME HERE! https://gist.github.com/eethann/3430971
 // // I am a mixin that allows _.filter to work with Objects.
 // _.mixin({
@@ -176,7 +212,3 @@
 //     makeTabs();
 //   });
 // }
-
-
-
-
